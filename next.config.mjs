034e36a826dbe60b1edab4aaf155c5909d25088a
Bala murugan L <bala060304@gmail.com>
2026-02-09 23:18:58 +0530

    Fixes (#15)
    * feat:Fixes
    
    * feat:Fixes
    
    * feat:Section
    
    * feat:Fixes
    
    * feat:Fixes
    
    * feat:Fixes
    
    * feat:Performance Optimization
    
    * feat:Fixes
    
    * feat:Performance Optimization
    
    * feat:Fixes
    
    * feat:CRM changes
    
    * api fixes
    
    ---------
    
    Co-authored-by: Bala <bala@entries.ai>

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
-        unoptimized: true, // Required for static export
+        unoptimized: true,
         formats: ['image/webp'], // Use WebP format
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
